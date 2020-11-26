ffdbeb0d3241d3ed465e609e867c9aaba2a4a08e
jwang0110 <johnw7@uci.edu>
2020-11-26 15:40:36 -0800

    adding backend

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex ({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        password : 'mypassword',
+        database : 'smart-brain'
+    }
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'john',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'sally',
+            email: 'bananas',
+            entries: '0',
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
+
+app.put('/image', (req, res) => {image.handleImage(req, res, db)});
+
+app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)});
+
+app.listen(3001, () => {
+    console.log("app is running");
+})
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> USER
+*/
