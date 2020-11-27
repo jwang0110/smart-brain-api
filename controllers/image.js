1723d3be2c13116159b62e1b79d1203d787f5149
jwang0110 <johnw7@uci.edu>
2020-11-26 20:45:51 -0800

    trying to fix api error

--- controllers/image.js
+++ controllers/image.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
     app.models.predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
         .then(data => res.json(data))
-        .catch(err => res.status(400).json('unable to work with API'));
+        .catch(err => res.status(400).json(err));
 };
 
 
