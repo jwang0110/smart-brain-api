93876f0c0532cbb67c5e7ca2cf448526142fb1ba
jwang0110 <johnw7@uci.edu>
2020-11-26 20:27:23 -0800

    add test case

--- controllers/register.js
+++ controllers/register.js
@@ -28,7 +28,7 @@ const handleRegister = (req, res, db, bcrypt) => {
             .catch(trx.rollback);
     })
     .catch((err) => {
-        res.status(400).json('unable to register');
+        res.status(400).json({name, email, password, hash});
     });
 }
 
